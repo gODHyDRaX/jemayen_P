c2bcb066755cf383b965949a7a8c99b301693f9d
gODHyDRaX <jemayen@scl.edu.gt>
2024-06-17 10:01:02 -0600

    correccion de proyectos

--- js/component_projects.js
+++ js/component_projects.js
@@ -23,12 +23,12 @@ let cp_projectos = `
     </div>
 </div>
 <div class="pj">
-    <img src="https://github.com/gODHyDRaX/img/blob/main/Captura%20de%20pantalla%202024-03-15%20104035.png?raw=true" class="imgP" alt="">
+    <img src="https://github.com/gODHyDRaX/img-P/blob/main/godhydrax.github.io_todo_list_.png?raw=true" class="imgP" alt="">
     <h2>Todo_list</h2>
     <span>Esta es una descripcion</span>
     <div class="btns">
-        <a href="https://github.com/gODHyDRaX/to-do_list_2024" class="btn1">Github</a>
-        <a href="https://godhydrax.github.io/to-do_list_2024/" class="btn2">View</a>
+        <a href="https://github.com/gODHyDRaX/todo_list" class="btn1">Github</a>
+        <a href="https://godhydrax.github.io/todo_list/" class="btn2">View</a>
     </div>
 </div>
 <div class="pj">
